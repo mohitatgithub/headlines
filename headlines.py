8b0c97acff07602e79254511cbf008f8d307908d
newmohit15 <mohitrathore15@gmail.com>
2017-03-20 11:43:06 +0530

    Using GET

--- headlines.py
+++ headlines.py
@@ -1,38 +1,24 @@
+from flask import render_template
 import feedparser
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-'''
-FULL_DISCOURSE = "http://seclists.org/rss/fulldisclosure.rss"
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
 def get_news():
-	#return "no news is good"
-	fedd = feedparser.parse(FULL_DISCOURSE)
-	first_article = feed['entries'][0]
-	return """<html>
-		<body>
-			<h1> Full discourse Headlines </h1>
-			<b>{0}</b> <br/>
-		</body>
-	</html>""".format(first_article.get("title"))
-'''
-
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
-@app.route("/")
-
-def get_news():
-	feed = feedparser.parse(BBC_FEED)
-	first_article = feed['entries'][0]
-	return """<html>
-		<body>
-			<h1> BBC Headlines </h1>
-			<b>{0}</b> <br/>
-			<i>{1}</i> <br/>
-			<p>{2}</p> <br/>
-</body>
-</html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+	query = request.args.get("publication")
+	if not query or query.lower() not in RSS_FEEDS:
+		publication = "bbc"
+	else:
+		publication = query.lower()
+	feed = feedparser.parse(RSS_FEEDS[publication])
+	return render_template("home.html",articles=feed['entries'])
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
